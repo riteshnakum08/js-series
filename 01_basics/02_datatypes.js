196c9dd9002f19be935d3a4d47e9b425f45f0726
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2024-12-25 15:19:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(2 + 5)//we are using nodejs not browser so it will not work for now
+
+//premitive datatypes
+//number =>2*(53)
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standalone value
+//undefined => value not assinged
+//symbol =>unique
+
+
+//objects
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
