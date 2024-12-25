d7028ac6bdfe23045eb4d0042994687ab27bfb58
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2024-12-25 15:19:28 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 12345
+let accountEmail = "riteshnakum001@gmail.com"
+var accountPassword = "HE@123"
+accountCity = "indore"
+
+
+// accountId = 234 //not allowed to written
+accountEmail = "ritesh1@gmail.com"
+ accountPassword = "H23"
+ accountCity = "bengluru" //not a good way to written it but sometimes its acceptable
+let accountState;
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+/*prefer not to use "var",
+because of issue in block scope and functional scope
+ */
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
+
+
