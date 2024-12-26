74099fa33cf9cb4cf5477b0cd24f8b20ff802227
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2024-12-26 07:49:17 +0000

    conversion of datatyp

---
+++ 01_basics/03_conversion_op.js
@@ -0,0 +1,26 @@
+let score = "true"
+
+// console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//conversions
+//"33" = 33
+//"23abc" = NaN
+// undefind = 
+//true = 1; false =0;
+
+let isLoggedIN = 1
+let booleanisLoggedIN = Boolean(isLoggedIN)
+//console.log(booleanisLoggedIN);
+
+//conversions
+//1 = true, 0 = false
+//"" =false
+//"dsGHA" = true 
+
+
+let someNum = "34"
+let stringNumber = String(someNum)
+console.log(stringNumber);
