91704fc43290b097b2d5b3dc61c957765861de76
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2024-12-27 08:42:53 +0000

    conversions

--- 01_basics/03_conversion_op.js
+++ 01_basics/03_conversion_op.js
@@ -23,4 +23,26 @@ let booleanisLoggedIN = Boolean(isLoggedIN)
 
 let someNum = "34"
 let stringNumber = String(someNum)
-console.log(stringNumber);
+//console.log(stringNumber);
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " ji"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log(+true);
+// console.log(+"");
+
+let counter =100
+counter++
+// console.log(counter);
+
+let x = 3;
+const y = ++x;
+console.log(x,y);
+
+
