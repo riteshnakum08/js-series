37a4ee1ddba824ff336e1acf570173f5768cf85b
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2024-12-27 08:43:17 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1);
+
+
+//we have to avoid such types of conversions
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("2" === "2");
