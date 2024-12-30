b0f3fa273520fe99ce2ea7f9bdc1439af313a27d
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2024-12-30 10:23:23 +0000

    number and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,35 @@
+const score  = 400
+// console.log(score);
+
+const balance  = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(4));
+
+const otherNum = 354.9867
+// console.log(otherNum.toPrecision(5));
+
+const hundreads = 1000000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+// -----------------------------MATHS-------------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-234));
+// console.log(Math.round(2.89766));
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(3.2));
+// console.log(Math.max(12,43,2,3,2,112,22,33));
+// console.log(Math.min(0,-1,4,6,3,2,65,4,365));
+
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min +1))+min);
+
+
+
