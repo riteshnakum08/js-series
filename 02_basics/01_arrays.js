9558ea246f174094558e922d88934ade5d11f5ef
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2025-01-02 15:12:11 +0000

    array_basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+//Arrays
+const myArr = [0,1,2,3,4,5,6]
+const myHero = ["sunny","danny","mia","leone"]
+const myArr2 = new Array(1,2,3,3,4,4,5)
+
+
+//arrays method
+// myArr.push(1,2,22,33)//used to add value in array
+// console.log(myArr);
+// myArr.pop()//used to remove last element of array
+// console.log(myArr);
+// myArr.unshift(89)
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(4536));
+// console.log(myArr.indexOf(4));
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+//slice , splice
+console.log("A" , myArr);
+const myNew1 = myArr.slice(0,3)
+console.log(myNew1);
+console.log("B", myArr);
+const myNew2 = myArr.splice(1,3)
+console.log(myNew2);
+
+
+
+
+
+
+ 
