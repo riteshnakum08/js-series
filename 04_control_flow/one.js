a45daeb45f890eca2dfbd888ddb680eb2fdd6258
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2025-01-08 16:04:08 +0000

    basics_control_statements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,33 @@
+//if
+// if (2 ==="2") {
+//    // console.log("executed");
+    
+// }else{
+//     //console.log("not executed");
+    
+// }
+ //  < , > , <= , >= , ==(equal) , != , ===(strict equal) , !==
+
+
+//  const score = 200
+//  if (score > 100) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+    
+//  }
+//  console.log(`User Power: ${power}`);
+ 
+// const balance = 1000
+// if (balance > 500) console.log("test");
+
+const UserLoggedin = true
+const debitCard = true
+if (UserLoggedin && debitCard && 2==3) {
+    console.log("Allowed for shopping");
+       
+}
+if (UserLoggedin || debitCard ) {
+    console.log("user loggedin  in second attemp");
+    
+}
+// && and , || or
