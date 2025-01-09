8f57ca6d8357415cf2365bbbeb0cbcdf5fa4c28b
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2025-01-09 16:22:31 +0000

    while , do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,22 @@
+
+
+let index = 0
+while (index <= 10) {
+    // console.log(index);
+    index++
+    
+}
+
+let myArr =["white" , "blue" , "pink" , "yello" , "red"]
+let arr = 0
+while (arr < myArr.length) {
+   // console.log(`some of the colors name are : ${myArr[arr]}`);
+    arr =  arr + 1
+}
+
+let marks = 1
+do{
+    console.log(`marks is ${marks}` );
+    marks++
+    
+}while(marks <= 10)
