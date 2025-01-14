583a4a3ff07a7b7cf8241477c3ce4ab6ce7d3f65
riteshnakum08 <121534631+riteshnakum08@users.noreply.github.com>
2025-01-14 15:39:08 +0000

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,14 @@
+const myObjects = {
+    Js : "JavaScript",
+    Cpp : "C++" ,
+    RB : "Ruby",
+}
+
+for (const key in myObjects) {
+  //console.log(`${key} shortcut of  ${myObjects[key]}`); 
+}
+
+const programm =["JS", "JAVA", "RB" ,"PYTHON" ,"CPP" ]
+for (const key in programm) {
+    console.log(programm[key]);   
+}
